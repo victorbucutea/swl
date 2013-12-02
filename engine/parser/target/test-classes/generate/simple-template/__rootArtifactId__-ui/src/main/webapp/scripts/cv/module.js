014e18d6048d72fdf61ea9283a2f44f85c0e6135
victor.bucutea@gmail.com <victor.bucutea@gmail.com>
2013-12-02 08:21:08 +0000

    removed .class files

--- engine/parser/target/test-classes/generate/simple-template/__rootArtifactId__-ui/src/main/webapp/scripts/cv/module.js
+++
@@ -1,9 +0,0 @@
-define([ 'angular', 
-         'cv/controller', 
-         'cv/cv-service'], 
-		function(angular, cvController , cvService) {
-			var module = angular.module('cv', ['common'])
-												 .controller('cvController',[ '$scope', '$http', 'cvService', cvController ])
-												 .config([ '$routeProvider', function($routeProvider) {$routeProvider.when('/cv', {templateUrl : 'html/cv/index.html'});}])
-												 .service('cvService', [ '$resource', 'messageService',  cvService]);
-});
